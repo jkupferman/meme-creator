bd81e7aca1081bd0c09855443f5dedf73a23c1bf
Jonathan Kupferman <jonathan@turntable.fm>
2012-09-27 10:51:22 -0400

    Whoops, looks like heroku doesn't have shellwords.

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require "rubygems"
 require "sinatra"
 require "tempfile"
 require "RMagick"
-require "Shellwords"
 
 AVAILABLE_MEMES = {
   "aliensguy" => {:name => "Aliens Guy", :width => 540},
@@ -62,7 +61,6 @@ end
 
 def convert text, source, destination, location, width
   fontpath = File.dirname(__FILE__) + "/lib/impact.ttf"
-  text = Shellwords.escape(text)
   cmd = "convert -fill white -stroke black -strokewidth 2 -background transparent -gravity center -size #{width}x -pointsize 56 -font #{fontpath} -weight Bold caption:\"#{text}\" #{source} +swap -gravity #{location} -composite #{destination}"
   result = `#{cmd}`
 end
